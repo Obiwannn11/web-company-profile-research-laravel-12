ea40b0957ddea17e20fad50abdf907d7d1fdd81d
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-09-30 20:05:43 +0800

    kelebihan kolom dan salah

--- database/migrations/2025_09_30_022416_create_faqs_table.php
+++ database/migrations/2025_09_30_022416_create_faqs_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('sort_order')->default(0);
-            $table->string('video_url')->nullable();
             $table->timestamps();
         });
     }
