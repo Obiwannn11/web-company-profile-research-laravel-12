e7deb9781dc3f44a65d040d3b7653eac32501c60
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-09-30 19:33:21 +0800

    atur fillable project, service dan translation nya

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'type', 'category', 'image', 'sort_order'];
 }
--- app/Models/ProjectTranslation.php
+++ app/Models/ProjectTranslation.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProjectTranslation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['project_id', 'locale', 'title', 'details', 'description'];
 }
