4782bca6208626933467a1d20cce359769c88854
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-10-05 16:43:01 +0800

    menambahkan service controller

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function index()
+    {
+        $services = Service::query()
+                    ->with('translations')
+                    ->orderBy('sort_order', 'asc')
+                    ->get();
+        return route('locale.services.index', compact('services'));
+    }
+
+    public function show($slug)
+    {
+        $services = Service::query()
+                    ->where('slug', $slug)
+                    ->with('translations')
+                    ->firstOrFail();
+
+        return route('locale.services.show', compact('services'));
+    }
 }
