670c068bb886f5a2c18088ac5d9a054d53e47996
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-10-05 15:20:43 +0800

    membuat route awal sementara

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,48 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ToolController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\PublicationController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home', ['locale' => 'id']);
+});
+
+// 2. Grup untuk semua route yang memiliki prefix bahasa
+Route::prefix('{locale}')
+    ->where(['locale' => '[a-z]{2}']) // Memastikan locale hanya 2 huruf (en, id, dll)
+    ->name('locale.') // Memberi awalan nama route, cth: locale.services.index
+    ->group(function () {
+
+    // Route untuk Home
+    Route::get('/', [PageController::class, 'home'])->name('home');
+
+    // Routes untuk Services
+    Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
+    Route::get('/services/{slug}', [ServiceController::class, 'show'])->name('services.show');
+
+    // Routes untuk R&D
+    Route::prefix('rd')->name('rnd.')->group(function() {
+        Route::get('/projects', [ProjectController::class, 'projects'])->name('projects');
+        Route::get('/research', [ProjectController::class, 'research'])->name('research');
+        Route::get('/publications', [PublicationController::class, 'publications'])->name('publications');
+    });
+
+    // Route untuk Tools
+    Route::get('/tools', [ToolController::class, 'index'])->name('tools.index');
+
+    // Routes untuk About Us
+    Route::prefix('about')->name('about.')->group(function() {
+        Route::get('/company', [AboutController::class, 'company'])->name('company');
+        Route::get('/team', [AboutController::class, 'team'])->name('team');
+        Route::get('/faq', [AboutController::class, 'faq'])->name('faq');
+    });
+
+    // Route untuk Contact
+    Route::get('/contact', [AboutController::class, 'index'])->name('contact.index');
+
 });
