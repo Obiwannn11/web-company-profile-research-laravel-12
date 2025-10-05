870cff40370ae3ccd23a72d97d300097f35be106
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-10-05 18:49:05 +0800

    memperbaiki use yang salah, menjadi facades view karena view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\View\View;
 use App\View\Composers\FooterComposer;
 use App\View\Composers\NavbarComposer;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         View::composer('components.navbar', NavbarComposer::class);
+         Facades\View::composer('components.navbar', NavbarComposer::class);
          View::composer('components.footer', FooterComposer::class);
     }
 }
