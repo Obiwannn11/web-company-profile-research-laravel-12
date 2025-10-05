20f9444f883d75d634657aab8a7e299dbdb4ce54
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-10-05 18:48:36 +0800

    menambahkan page controller untuk home

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function home()
     {
-        return route('locale.home');
+
+         // Ambil 3 layanan pertama yang dibuat, beserta terjemahannya
+        $featuredServices = Service::query()
+            ->with('translations')
+            ->orderBy('created_at', 'asc')
+            ->limit(3)
+            ->get();
+
+        return route('locale.home', compact('featuredServices'));
     }
 }
