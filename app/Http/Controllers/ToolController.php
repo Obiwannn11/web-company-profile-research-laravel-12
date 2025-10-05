3b79651306df52427e31c5877047ad031e8ebdc2
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-10-05 15:48:46 +0800

    menambahkan tool controller

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tool;
 use Illuminate\Http\Request;
 
 class ToolController extends Controller
 {
-    //
+    
+    public function index()
+    {
+
+        $tools = Tool::query()
+        ->with('translations')
+        ->orderBy('created_at', 'asc')
+        ->get();
+        return route('locale.tools.index', compact('tools'));
+    }
 }
