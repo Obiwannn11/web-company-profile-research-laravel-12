200526c881665e0059500baefa5318f404085de4
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-10-27 18:59:52 +0800

    menambahkan rnd menjadi dropdown dengan 3 item nya

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 {{-- x-data mendefinisikan "state" untuk komponen ini --}}
 {{-- servicesOpen dan aboutOpen awalnya 'false' (tertutup) --}}
-<header x-data="{ open: false, servicesOpen: false, aboutOpen: false }" class="bg-white shadow-md sticky top-0 z-50">
+<header x-data="{ open: false, servicesOpen: false, aboutOpen: false, rndOpen: false }" class="bg-white shadow-md sticky top-0 z-50">
     <nav class="container mx-auto px-6 py-4">
         <div class="flex items-center justify-between">
             <a href="{{ route('locale.home', ['locale' => app()->getLocale()]) }}" class="text-xl font-bold text-blue-600">
@@ -33,7 +33,20 @@
                     </div>
                 </div>
 
-                <a href="{{ route('locale.rnd.projects', ['locale' => app()->getLocale()]) }}" class="text-gray-700 hover:text-blue-600">{{ $navbarItem->get('navbar_rnd', 'R&D Projectss') }}</a>
+                {{-- <a href="{{ route('locale.rnd.projects', ['locale' => app()->getLocale()]) }}" class="text-gray-700 hover:text-blue-600">{{ $navbarItem->get('navbar_rnd', 'R&D Projectss') }}</a> --}}
+
+                <div class="relative">
+                    <button @click="rndOpen = !rndOpen" class="flex items-center text-gray-700 hover:text-blue-600">
+                        {{ $navbarItem->get('navbar_rnd', 'Navbar RND') }}
+                        <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                    </button>
+                    <div x-show="rndOpen" @click.away="rndOpen = false" x-transition class="absolute mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-20" style="display: none;">
+                        <a href="{{ route('locale.rnd.research', ['locale' => app()->getLocale()]) }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50">Research</a>
+                    <a href="{{ route('locale.rnd.projects', ['locale' => app()->getLocale()]) }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50">Projects</a>
+                        <a href="{{ route('locale.rnd.publication', ['locale' => app()->getLocale()]) }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50">Publication</a>
+                    </div>
+                </div>
+
                 <a href="{{ route('locale.tools.index', ['locale' => app()->getLocale()]) }}" class="text-gray-700 hover:text-blue-600">{{ $navbarItem->get('navbar_tools', 'Toools') }}</a>
 
                 {{-- Dropdown About Us (statis) --}}
