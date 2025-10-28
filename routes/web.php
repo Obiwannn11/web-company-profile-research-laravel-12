0245a08f0e30514dc4730a846f63196e7ee6067a
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-10-28 18:06:18 +0800

    menambahkan s pada route dan methodnya

--- routes/web.php
+++ routes/web.php
@@ -137,7 +137,7 @@ Route::group([
         Route::prefix('rnd')->name('rnd.')->group(function() {
             Route::get('/projects', [RndController::class, 'projects'])->name('projects');
             Route::get('/research', [RndController::class, 'research'])->name('research');
-            Route::get('/publication', [RndController::class, 'publication'])->name('publication');
+            Route::get('/publications', [RndController::class, 'publications'])->name('publication');
         });
 
         // Route untuk Tools
