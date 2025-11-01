7fc11561f3d20dacf4c9a515540ee5be00657a8c
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-11-01 18:49:59 +0800

    membuat fungsi untuk pengecekan translation dan link url yang final  nanti

--- app/Models/Carousel.php
+++ app/Models/Carousel.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use App\Models\CarouselTranslation;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Carousel extends Model
 {
@@ -18,4 +20,46 @@ class Carousel extends Model
     {
         return $this->hasMany(CarouselTranslation::class);
     }
+
+    protected function currentTranslation(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value) {
+                $translation = $this->translations->firstWhere('locale', app()->getLocale());
+                return $translation ?? (object)[
+                    'title' => '',
+                    'subtitle' => '',
+                    'button_text' => '',
+                ];
+            },
+        );
+    }
+
+    protected function finalUrl(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value) {
+                $url = $this->link_url; 
+                
+                if (!$url) {
+                    return '#';
+                }
+
+                // Cek jika link eksternal misal ada http
+                if (Str::startsWith($url, 'http')) {
+                    return $url;
+                }
+
+                // Cek jika link internal misal /
+                if (Str::startsWith($url, '/')) {
+                    // Otomatis tambahkan locale yang aktif
+                    return url(app()->getLocale() . $url);
+                }
+
+                return $url;
+            },
+        );
+    }
+
+    
 }
